67ff0db9c8e16e5f2b46b99e9e91dfdc2f8ebb2b
Pablo Alpizar <pabloalpizar99@gmail.com>
2021-10-27 16:32:34 -0600

    add exmaple of the shared memory

--- Project_2/spy.c
+++ Project_2/spy.c
@@ -8,6 +8,11 @@
 #include <stdio.h>
 
 
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include "memory_line.h"
+
+
 // functions definition
 int showMenu();
 
@@ -28,9 +33,29 @@ int main() {
     // variables
     int option;
 
-    do {
+    // ftok to generate unique key
+    key_t key = ftok("shmfile",65);
+
+
+   // shmget returns an identifier in shmid
+    int shmid = shmget(key,sizeof(MemoryLine),0666|IPC_CREAT);
+  
+    // shmat to attach to shared memory
+    MemoryLine *lines = (MemoryLine*) shmat(shmid,(void*)0,0);
+  
+    printf("Data read from memory: %d, %ld, %d\n",lines[0].available, lines[0].pid, lines[0].lineNumber);
+    printf("Data read from memory: %d, %ld, %d\n",lines[1].available, lines[1].pid, lines[1].lineNumber);
+
+
+    //detach from shared memory 
+    shmdt(lines);
+
+    // destroy the shared memory
+    shmctl(shmid,IPC_RMID,NULL);
+
+    /*do {
         option = showMenu();
-    }while (option != 3);
+    }while (option != 3);*/
     
     return 0;
 }
