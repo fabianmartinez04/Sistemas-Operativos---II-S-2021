e3b39f66ca3b4764ba0276a6f4175daacd777417
Pablo Alpizar <pabloalpizar99@gmail.com>
2021-10-31 00:07:17 -0600

    add defines values file

---
+++ Project_2/defines_values.h
@@ -0,0 +1,15 @@
+#ifndef DEFINE_VALUES_H
+#define DEFINE_VALUES_H
+
+// define constant values
+
+// range of the thread size
+#define LINE_RANGE 1,10
+// range of the execute thread time
+#define THREAD_EXECUTE_TIME 20,60
+// range of the time to generate new thread
+#define THREAD_TIME 30,60
+// max thread in thread list
+#define MAX_THREAD 100;
+
+#endif
