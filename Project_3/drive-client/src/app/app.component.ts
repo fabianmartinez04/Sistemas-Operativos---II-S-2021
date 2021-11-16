376cf98b96ade1f2be30679f50ba3ac70d93d573
palpizarm <pabloalpizar99@gmail.com>
2021-11-16 00:44:57 -0600

    add client and server project

---
+++ Project_3/drive-client/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { WebSocketAPI } from './models/WebSocketsAPI';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'drive-client';
+
+  webSocketAPI: WebSocketAPI =  new WebSocketAPI(this);;
+  greetings: string[];
+  name: string = '';
+
+
+  connect(){
+    this.webSocketAPI._connect();
+  }
+
+  disconnect(){
+    this.webSocketAPI._disconnect();
+  }
+
+  sendMessage(){
+    this.webSocketAPI._send(this.name);
+  }
+
+  handleMessage(message:any){
+    this.greetings.push(message);
+  }
+}
